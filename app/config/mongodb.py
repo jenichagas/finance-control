6d33972e92973d881f9bd16e8436480c2fd8c928
Jeniffer <jenif.work@gmail.com>
2025-04-14 15:15:32 -0300

    🎉 structure project

---
+++ app/config/mongodb.py
@@ -0,0 +1,7 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+
+client = AsyncIOMotorClient(MONGO_URL)
+db = client("finance_control")
